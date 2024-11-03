3c4f720e0356dc68b141446119f4befb0627ebb1
Mathis Boulais <mathis.oxym@gmail.com>
2024-11-03 17:06:00 +0100

    Changement de l'arborescence du projet

---
+++ cmd/crm/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"tp1/internal/handler"
+)
+
+func main() {
+	afficherMenu()
+}
+
+func afficherMenu() {
+	for {
+		fmt.Println("\n=== Mini-CRM ===")
+		fmt.Println("1. Ajouter un contact")
+		fmt.Println("2. Lister tous les contacts")
+		fmt.Println("3. Supprimer un contact")
+		fmt.Println("4. Mettre à jour un contact")
+		fmt.Println("5. Quitter")
+		fmt.Print("\nChoisissez une option: ")
+
+		var choix string
+		fmt.Scanln(&choix)
+
+		switch choix {
+		case "1":
+			handler.AjouterContact()
+		case "2":
+			handler.ListerContacts()
+		case "3":
+			fmt.Println("Suppression d'un contact...")
+		case "4":
+			fmt.Println("Mise à jour d'un contact...")
+		case "5":
+			fmt.Println("Au revoir!")
+			return
+		default:
+			fmt.Println("Option invalide, veuillez réessayer.")
+		}
+	}
+}
